279574d95b70acec50fd8fd5d6ea457e92595b37
Hector Zhao <hector.zhao@fluxfederation.com>
2018-06-22 13:46:10 +1200

    Add validations on Profuct. (Following the tutorial...boring...)

--- app/models/depot/product.rb
+++ app/models/depot/product.rb
@@ -4,6 +4,15 @@ module Depot
   class Product < ApplicationRecord
     include NullifyBlankValues
 
+    validates :description, presence: true
+    validates :image_url, allow_blank: true,
+              format: { with: /\.(gif|jpg|jpeg|png)\z/i, message: "must be a URL for GIF, JPG, JPEG, or PNG image" }
+
+    validates :title, presence: true, uniqueness: true
+    validates :price, presence: true,
+              format: { with: /\A\d+(?:\.\d{2})?\z/ },
+              numericality: { greater_than_or_equal_to: 0.00, less_than: 1000000 }
+
     before_save :nullify_blank_values
   end
 end
